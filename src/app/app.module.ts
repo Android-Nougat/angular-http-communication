6a51127dceec37068f85f17a39df7bfbdadfca29
adhishreya <adhishreya15081947@gmail.com>
2022-08-20 01:29:06 +0530

    interceptor v-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser'; 
+import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AddBookComponent } from './add-book/add-book.component';
 import { AddReaderComponent } from './add-reader/add-reader.component';
@@ -12,6 +12,8 @@ import { EditBookComponent } from './edit-book/edit-book.component';
 import { EditReaderComponent } from './edit-reader/edit-reader.component';
 import { BookTrackerErrorHandlerService } from './core/book-tracker-error-handler.service';
 
+import { AddHeaderInterceptor } from './core/add-header.interceptors';
+import { LogResponseInterceptor } from './core/log-response.interceptors';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { BookTrackerErrorHandlerService } from './core/book-tracker-error-handle
     AddReaderComponent
   ],
   providers: [
-    { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService }
+    { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService },
+    { provide: HTTP_INTERCEPTORS, useClass: AddHeaderInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LogResponseInterceptor, multi: true },
   ],
   imports: [
     BrowserModule,
